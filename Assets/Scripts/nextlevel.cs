5ad7e40c28228d8b35fe05b063f99438fbcf5635
Alexminayo <alexminayo90@gmail.com>
2014-12-03 18:57:39 +0100

    puertas

---
+++ Assets/Scripts/nextlevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextlevel : MonoBehaviour {
+
+	public string nombreNivel = "zombie3";
+
+		bool cargando = false;
+
+	void OnTriggerStay2D(Collider2D target){
+
+		if (target.transform.tag == "Player") {
+			Application.LoadLevel(nombreNivel);
+		}
+	}
+}
