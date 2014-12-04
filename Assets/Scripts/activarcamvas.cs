3d8efe300ee55fbc91af551d2f852f508d594359
Alexminayo <alexminayo90@gmail.com>
2014-12-04 19:42:25 +0100

    menu

---
+++ Assets/Scripts/activarcamvas.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activarcamvas : MonoBehaviour {
+	public GameObject canvas;
+	public bool estacerca = false;
+	public bool muestratexto = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		estacerca = true;
+		}
+
+	void OnTriggerExit2D(Collider2D col){
+				estacerca = false;
+		}
+	// Update is called once per frame
+	void Update () {
+	if (estacerca) {
+			canvas.SetActive (true);
+				}else{
+			canvas.SetActive(false); 
+	}
+}
+}
